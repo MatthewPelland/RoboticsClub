a80f3a18663688ad7dbbf8ed749f521e7ed6e756
16lheinold <lilibug156@gmail.com>
2017-02-01 18:46:03 -0500

    Drive Library .h

---
+++ trinityRobot2016/wheel.h.ino
@@ -0,0 +1,30 @@
+#ifndef wheel_h
+#define wheel_h
+#include "Arduino.h"
+
+class Wheel {
+  public: 
+    Wheel(int eP, float ROBOT_RADIUS)
+    driveInches(int inches, int power, int motor);
+    drive(float rad, float inches);
+
+  private:
+    int encoderValue;
+    int encoderPin;
+    int lastEncoder;
+    int currentEncoder;
+    updateEncoder();
+    inchesToEncoder(int inches);
+    encoderToInches(int encoder);
+	float PID(float current, float setpoint, float KI, float KP, float KD, float MAX_OUTPUT);
+	float distance(float xi, float yi, float xf, float yf);
+	float angle(float xi, float yi, float xf, float yf);
+	float distancex(float rad);
+	float distancey(float rad);
+	float getLinearSpeedEncoder();
+	float getAngularSpeedEncoder();
+	float getSpeedAccel();
+	float getSpeedGyro();
+};
+
+#endif
